801dee54f370a180f961cc2b0bc7820ba8809ae6
OrShazam <50451273+OrShazam@users.noreply.github.com>
2021-12-15 20:57:07 +0200

    Add files via upload

--- ChainEngine/main.c
+++ ChainEngine/main.c
@@ -2,8 +2,11 @@
 #include "scrap.h"
 #include "console.h"
 #include "utils.h"
+#include "string.h"
+
+#define RET 0xC3
 void PrintUsage(){
-	printf("Usage: ntdllscraper.exe <bytes (hex)>\n");
+	printf("Usage: ChainEngine.exe <inst;inst...>\n");
 }
 int main(int argc, char* argv[]){
 	if (argc < 2){
@@ -11,24 +14,59 @@ int main(int argc, char* argv[]){
 		return 0;
 	}
 	int len;
-	BYTE* bytes = HexTranslateSafe(argv[1], &len);
-	for (int i = 0; i < len; i++){
+	char* text = argv[1];
+	TokenizeSemicolons(text);
+	PBYTE buffers[50]; int i =0, j, idx=0,prevIdx;
+	BYTE ROPCHAIN[1024];
+	memset(&ROPCHAIN[0],0,1024);
+	int lengths[50];
+	while (*text != '\n' && i < 50){
+		len = strlen(text);
+		text[len] = (char)RET;
+		buffers[i++] = HexTranslateSafe(text,&lengths[i]);
+		text += len + 2;
 	}
-	if (bytes == NULL){
-		PrintError("Couldn't translate bytes\n");
-		PrintUsage();
-		return 1;
+	j = --i;
+	for (; i >= 0; i--){
+		if (buffers[i] == NULL && lengths[i] != 0){
+			PrintError("Couldn't translate bytes\n");
+			PrintUsage();
+			return 1;
+		}
 	}
 	ScraperData data;
 	FillScraperData(&data, "ntdll.dll");
 	LPVOID address;
-	if (FindBytes(&data,bytes,len,&address)){
-		printf("Found byte sequence at address at %p\n", address);
+	for (int i = 0; i < j; i++){
+		if (lengths[i] > 0){
+			if (FindBytes(&data,buffers[i],len,&address)){
+				printf("Found byte sequence '%x...RET' at address %p\n", *buffers[i], address);
+			}
+			else {				
+				PrintError("Couldn't find byte sequence '%x...'\n", *buffers[i]);
+				goto cleanup;
+			}
+			prevIdx = idx;
+			for (; idx < prevIdx + sizeof(LPVOID) / sizeof(BYTE); idx++){
+				
+				ROPCHAIN[idx] = (BYTE)(((ULONGLONG)address >> (8 * idx)) & 0xff);
+			}
+		}
 	}
-	else 
-		PrintError("Couldn't find byte sequence\n");
-	
-	HeapFree(GetProcessHeap(),
-		0,
-		bytes);
+	len = strlen(&ROPCHAIN[0]);
+	printf("char ROPCHAIN[] =\n");
+	for (int i = 0; i < len; i++){
+		if (i % 16 == 0){
+			printf("\n");
+		}
+		printf("\\x%x",ROPCHAIN[i]);
+	}
+	cleanup:
+	for (int i = 0; i < j; i++){
+		if (buffers[i] != NULL){
+			HeapFree(GetProcessHeap(),
+				0,
+				buffers[i]);
+		}
+	}
 }
