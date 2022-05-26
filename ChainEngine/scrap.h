953c63d700f9270d0400f0b34da733d12ee78b7e
OrShazam <50451273+OrShazam@users.noreply.github.com>
2022-05-26 09:04:39 -0700

    Update scrap.h

--- ChainEngine/scrap.h
+++ ChainEngine/scrap.h
@@ -1,10 +1,12 @@
 
 #include "common.h"
+#include "string.h"
 typedef struct {
 	HMODULE hModule;
-	LPVOID Base;
-	ULONGLONG ImageSize;
+	LPVOID SearchBase;
+	ULONG SearchSize;
 } ScraperData, *PScraperData;
 
 void FillScraperData(PScraperData, const char*);
+VOID FindCodeSection(LPVOID imageBase, LPVOID* start, ULONG* size)
 BOOL FindBytes(PScraperData, BYTE*, size_t,LPVOID*);
